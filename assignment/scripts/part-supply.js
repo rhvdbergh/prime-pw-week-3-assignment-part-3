b58db7c87f80ac12242c06772507013f74e91226
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-08-31 07:27:00 -0500

    add console log to question 5

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -26,6 +26,7 @@ console.log(removedItem);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 supplyChanges.push(25);
+console.log('Just to be safe, logging the supplyChanges array here,', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
