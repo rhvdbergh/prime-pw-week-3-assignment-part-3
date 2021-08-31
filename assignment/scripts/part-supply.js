0877830039ed17f250242972578c4ac03fa8d913
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-08-31 07:28:21 -0500

    answers question 7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -49,6 +49,15 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
+for (value of supplyChanges) {
+  if (value > 0) {
+    console.log(`Added ${value} parts.`);
+  } else if (value === 0) {
+    console.log(`No Change.`);
+  } else { // the only possible value left is negative
+    console.log(`Removed ${value} parts.`);
+  }
+}
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
